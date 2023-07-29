e485f70bb68411384abc2178eaf049515759276d
Кристина Щитченко <alina.andreeva202@mail.ru>
2023-07-29 08:41:22 +0300

    a

---
+++ src/pages/Application/data.js
@@ -0,0 +1,20 @@
+export const banks = [
+    {
+        logo: './logos/logo_center_invest.svg',
+        stavka: '13%',
+        limit: 'до 300 000 ₽',
+        oplata: 'от 2 680 ₽ / месяц',
+        conditions: ['без залога', 'требуется подтверждение дохода'],
+        options: {
+            percent: "от 13%",
+            possibleAmount: 'до 300 тыс. ₽',
+            repaymentPeriod: '1 год - 4 года',
+            goal: 'Образование',
+            formsDoc: 'Ваш счет на нашем сервсие / Счет в вашем банке / Счет в банке, который выдает кредит',
+            reviewPeriod: 'До 2-х рабочих дней',
+            orderRegistration: 'В личном кабинете нашего сервиса',
+            proofIncome: 'Справка 2-НДФЛ',
+            
+        }
+    }
+]
