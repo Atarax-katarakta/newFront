c689429623685e139a1f9a1757571ccd1b33654d
Daskis <abramoff.job@gmail.com>
2023-07-30 13:34:33 +0300

    build 1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.scss';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -12,12 +12,12 @@ import {store} from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
 
             <App/>
         </Provider>
 
-    </BrowserRouter>
+    </HashRouter>
 );
 
